e04f93923e3f59d1aaa76d49dcae2389417485d4
IgorMihailets <igormihailets@gmail.com>
2025-09-16 14:36:59 +0200

    #25103 - Add DebtorEntryType

---
+++ src/Likvido.Domain.Enums/DebtorEntryType.cs
@@ -0,0 +1,11 @@
+﻿namespace Likvido.Domain.Enums
+{
+    public enum DebtorEntryType
+    {
+        Invoice = 0,
+        FinancialEntry = 2,
+        Payment = 3,
+        CreditNote = 4,
+        Fee = 5,
+    }
+}
